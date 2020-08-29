96a8ca20066460fdee8206a8883bf2d15cd03c1f
ACXap <acxap@list.ru>
2020-08-29 09:36:18 +0700

    Новая структура сместился индекс и улица

--- address/addressAttributeHouse.js
+++ address/addressAttributeHouse.js
@@ -6,7 +6,7 @@ class AddressAttributeHouse extends AddressAttribute {
 
     _setDiv() {
         const divs = document.querySelectorAll("div.col-lg-12.col-md-12.col-sm-12.col-xs-12");
-        this._div = divs[8];
+        this._div = divs[10];
 
         this._setDivDefault();
     }
--- address/addressAttributeTypeAddress.js
+++ address/addressAttributeTypeAddress.js
@@ -6,7 +6,7 @@ class AddressAttributeTypeAddress extends AddressAttribute {
 
     _setDiv() {
         const divs = document.querySelectorAll("div.col-lg-12.col-md-12.col-sm-12.col-xs-12");
-        this._div = divs[6];
+        this._div = divs[8];
 
         this._setDivDefault();
     }
